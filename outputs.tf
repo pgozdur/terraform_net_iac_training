632c14a21a9ec99c362a5180d583deee56c6c249
pgozdur <85687234+pgozdur@users.noreply.github.com>
2024-02-08 20:15:56 +0100

    corrected output file

--- outputs.tf
+++ outputs.tf
@@ -5,15 +5,15 @@ output "instance_external_dns" {
 
 output "instance_tags" {
   value = [for instance in aws_instance.lab_instance : instance.tags["Name"]]
-  description = "The tags of the EC2 instances"
+  description = "The tags (e.g., student_1) of the EC2 instances"
 }
 
 output "instance_public_ips" {
-  value = { for instance in aws_instance.lab_instance : instance.tags["Name"] => instance.public_ip => instance }
+  value = { for instance in aws_instance.lab_instance : instance.tags["Name"] => instance.public_ip }
   description = "The public IP addresses of the EC2 instances"
 }
 
 output "ssh_commands" {
-  value = [for instance in aws_instance.lab_instance : instance.tags["Name"] "ssh -i ${path.module}/${var.key_name} ubuntu@${instance.public_ip}"]
-  description = "Commands to SSH into each EC2 instance. Ensure your private key permissions are set correctly."
+  value = [for instance in aws_instance.lab_instance : instance.tags["Name"] "ssh -i /path/to/your/private/key.pem ubuntu@${instance.public_ip}"]
+  description = "Commands to SSH into each EC2 instance. Replace '/path/to/your/private/key.pem' with the actual path to your SSH private key."
 }
